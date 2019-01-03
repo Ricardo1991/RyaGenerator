2eca3b2a2cf21291e1353bad40121a525c080cdb
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2019-01-03 05:49:48 +0000

    Initial commit

---
+++ RyaGenerator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RyaGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RyaGenerator ryaGenerator = null;
+            bool exit = false;
+
+            while (exit == false)
+            {
+                Console.WriteLine("1 - Create rya file from HexChat log");
+                Console.WriteLine("2 - Load rya file");
+                Console.WriteLine("3 - Generate rya saying");
+                Console.WriteLine("4 - Exit");
+                Console.Write("\nOption: ");
+
+                string result = Console.ReadLine();
+
+                switch (result)
+                {
+                    case "1":
+                        Console.Write("File Path: ");
+                        result = Console.ReadLine();
+                        RyaGenerator.hexLogToRya(result);
+                        break;
+
+                    case "2":
+                        ryaGenerator = new RyaGenerator();
+                        bool loaded = ryaGenerator.loadRyaFile();
+                        if (loaded) Console.WriteLine("File Loaded!");
+                        break;
+
+                    case "3":
+                        if (ryaGenerator != null)
+                        {
+                            string ryaS = ryaGenerator.getRandomRya();
+                            Console.WriteLine("\n" + ryaS);
+                        }
+                        break;
+
+                    case "4":
+                        exit = true;
+                        break;
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ RyaGenerator/RyaGenerator.cs
@@ -0,0 +1,89 @@
+﻿using MarkovSharp.TokenisationStrategies;
+using System;
+using System.IO;
+
+namespace RyaGenerator
+{
+    internal class RyaGenerator
+    {
+        private StringMarkov stringMarkov;
+
+        private bool isReady = false;
+
+        public bool loadRyaFile()
+        {
+            stringMarkov = new StringMarkov();
+
+            if (File.Exists("TextFiles/rya.txt"))
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader("TextFiles/rya.txt");
+                    while (sr.Peek() >= 0)
+                    {
+                        string ryaL = sr.ReadLine().ToLower();
+                        if (ryaL.Split().Length > 2)
+                            stringMarkov.Learn(ryaL);
+                    }
+
+                    sr.Close();
+
+                    isReady = true;
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Reading File: " + ex.Message);
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+                return false;
+            }
+        }
+
+        public string getRandomRya()
+        {
+            if (isReady)
+                return stringMarkov.RebuildPhrase(stringMarkov.Walk());
+            else return null;
+        }
+
+        public static void hexLogToRya(string filePath)
+        {
+            StreamWriter writter = new StreamWriter("TextFiles/rya.txt");
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(filePath);
+                    while (sr.Peek() >= 0)
+                    {
+                        string ryaL = sr.ReadLine();
+                        string[] ryaSplit = ryaL.Split(new char[] { ' ' }, 4);
+
+                        if (ryaSplit.Length > 3 && ryaSplit[3].ToLower().StartsWith("<rya>"))
+                        {
+                            writter.WriteLine(ryaSplit[3]);
+                        }
+                    }
+
+                    sr.Close();
+                    writter.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Reading File: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+            }
+        }
+    }
+}
